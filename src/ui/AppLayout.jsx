b98910148de785c33ab80b34307ab4c1619f898d
obaidraza022 <obaidraza772@gmail.com>
2025-07-10 16:10:49 +0530

    first commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,30 @@
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import styled from "styled-components";
+
+const SyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <SyledAppLayout>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </SyledAppLayout>
+  );
+}
+
+export default AppLayout;
